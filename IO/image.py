1845643c73b765dbbccca1456887afe5a8da79d0
hannahjocelyn <h.j.brown@bradford.ac.uk>
2015-03-11 17:23:37 +0000

    Modified IO.image and PCA
    Included mask in PCA.
    Used IO.image to save PCA.

--- IO/image.py
+++ IO/image.py
@@ -6,7 +6,7 @@ Created on Wed Mar 11 16:15:46 2015
 """
 from PIL import Image
 import numpy as np
-def lum2png(comp,Max,Min,filename):
+def lum2png(comp,filename):
     L = 255
     A = np.zeros(np.shape(comp))
     A.fill(255)
@@ -14,19 +14,20 @@ def lum2png(comp,Max,Min,filename):
     masked = np.ma.masked_array(comp,np.isnan(comp))
     masked2 = np.ma.masked_array(A, mask=np.isnan(comp), fill_value=0)
     masked2 = masked2.filled(0)
-    masked2 = np.flipud(masked2)
+    #masked2 = np.flipud(masked2)
 
-    comp =  np.clip(masked, float(Min), float(Max), out=masked)
-    comp = np.flipud(comp)
-    Min = comp.min()
-    Max = comp.max()
+    np.array(masked)
+    #comp = np.flipud(comp)
+    Min = np.nanmin(comp)
+    Max = np.nanmax(comp)
+    print Min, Max
     Range = float(Max - Min)
     scale = 255.0/Range
     comp = comp - Min
     comp = comp*scale
     comp = comp + (255 - 2*comp)
 
-    l  = comp.filled(L)
+    l  = comp
 
     new_comp = np.array([l,masked2])
     new_comp = np.rollaxis(new_comp,-1)
--- Processing/PCA.py
+++ Processing/PCA.py
@@ -10,6 +10,8 @@ import numpy as np
 import pylab
 import os
 
+from ArchaeoPY.IO.image import lum2png
+
 def pca(X):
   # Principal Component Analysis
   # input: X, matrix with training data as flattened arrays in rows
@@ -38,23 +40,37 @@ def pca(X):
 
   #return the projection matrix, the variance and the mean
   return V,S,mean_X
-  
+
+#List of Images to combine (must be 'same' tile)  
 imlist = ('lidar_01_01.png', 'lidar2_01_01.png')
+#changes directory to 'Data' directory
 os.chdir('../../Data')
 
-im = np.array(Image.open(imlist[0])) #open one image to get the size
+#opens image to get image size
+im = np.array(Image.open(imlist[0]))
+#open one image to get the size
 m,n = im.shape[0:2] #get the size of the images
+
+#makes a copy of original image touse as mask later
+#255 appears to be 'nodata'
+mask = np.copy(im)
+
 imnbr = len(imlist) #get the number of images
 
 #create matrix to store all flattened images
-immatrix = np.array([np.array(Image.open(imlist[i])).flatten() for i in range(imnbr)],'f')
+immatrix = np.array([np.array(Image.open(imlist[i]),dtype=float).flatten() for i in range(imnbr)],'f')
 
 #perform PCA
 V,S,immean = pca(immatrix)
+print np.shape(V)
+print S
 
 #mean image and first mode of variation
 immean = immean.reshape(m,n)
 mode = V[0].reshape(m,n)
+
+#masks PDA'd data using mask from earlier
+mode[mask==255]=np.nan
 
 #show the images
 pylab.figure()
@@ -63,11 +79,16 @@ pylab.imshow(immean)
 
 pylab.figure()
 pylab.gray()
+pylab.imshow(V[1].reshape(m,n))
+
+pylab.figure()
+pylab.gray()
 pylab.imshow(mode)
 
 pylab.show()
-print mode
-immode = Image.fromarray(mode)
+
+#saves data using archaeopy lum2png
 fname = 'mode'+imlist[0]
-immode.save(fname)
+lum2png(mode,fname)
+
 
