d501e744c290a04d8ab7e5360db264709a3a2469
popefinn <popefinn@gmail.com>
2015-03-11 16:19:39 +0000

    Added Image saving module
    Only includes luminosity to png

---
+++ IO/image.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 11 16:15:46 2015
+
+@author: FPopecarter
+"""
+from PIL import Image
+import numpy as np
+def lum2png(comp,Max,Min,filename):
+    L = 255
+    A = np.zeros(np.shape(comp))
+    A.fill(255)
+
+    masked = np.ma.masked_array(comp,np.isnan(comp))
+    masked2 = np.ma.masked_array(A, mask=np.isnan(comp), fill_value=0)
+    masked2 = masked2.filled(0)
+    masked2 = np.flipud(masked2)
+
+    comp =  np.clip(masked, float(Min), float(Max), out=masked)
+    comp = np.flipud(comp)
+    Min = comp.min()
+    Max = comp.max()
+    Range = float(Max - Min)
+    scale = 255.0/Range
+    comp = comp - Min
+    comp = comp*scale
+    comp = comp + (255 - 2*comp)
+
+    l  = comp.filled(L)
+
+    new_comp = np.array([l,masked2])
+    new_comp = np.rollaxis(new_comp,-1)
+    new_comp = np.rollaxis(new_comp,-1)
+
+    #print new_comp.shape
+    im = Image.fromarray(np.uint8(new_comp), "LA" )
+    
+    png_info = im.info
+
+    im.save(filename, quality=100, **png_info)
